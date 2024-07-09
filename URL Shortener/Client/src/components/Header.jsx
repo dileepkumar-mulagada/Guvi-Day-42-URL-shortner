dfd1270cf04f8bf6f3f700ad1ef00aafc69e3d72
dileepkumar-mulagada <162285455+dileepkumar-mulagada@users.noreply.github.com>
2024-07-09 22:36:30 +0530

    Delete URL Shortener directory

--- URL Shortener/Client/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logo from '../images/logo.svg'
-
-export default function Header() {
-    return <header className="header padding-xy">
-        <div className="logo">
-            <img src={logo} alt="logo" />
-        </div>
-    </header>
-}
